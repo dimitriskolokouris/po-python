8032a21de74eaaca6be49ba71703884b258d1e9d
Sennvdheiden <95484351+Sennvdheiden@users.noreply.github.com>
2022-06-03 13:17:22 +0000

    vraag gemaakt en woordenlijst toegevoegd

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import random
-wordlist =
-["programmeren","galgje","quiz","woordenlijst","chocoladereep"]
+word_list = ["informatica","galgje","python","programmeren","maartenscollege"]
 
-def get_word(wordlist):
- word = random.choice(wordlist) 
- return word.upper()
+def getword(word_list):
+  word = random.choice(word_list)
+  return word.upper()
 
 
 def question():
   x = 0
-   while x < 2:
-     
+  while x < 2:
+    answer = input("Hallo speler, wil je een potje Galgje spelen? (ja of nee)")
+    if any(answer.lower() == f for f in ["ja"]):
+      print("Veel speelplezier!")
+      break
+    
+    else:
+      x += 1
+      if x < 2:
+        print("Weet je het zeker?")
+
+
+question()
+      
