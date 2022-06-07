e52e60d490a717776e9ff80e50be2a88e69b9288
Dimitris Kolokouris <95089320+dimitriskolokouris@users.noreply.github.com>
2022-06-07 14:20:45 +0000

    Introductiescherm toegevoegd

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 #Woordenlijst
 import random
+
 word_list = ["informatica","informatiekunde","spelletje","aardigheidje","scholier","fotografie","waardebepaling","specialiteit","verzekering","universiteit","heesterperk"]
+
+def get_word(word_list):
+  word = random.choice(word_list)
+  return word.upper()
+
+#Introductiescherm
+def question():
+  i = 0 
+  while i < 2:
+   answer = input("Welkom! Wil je galgje spelen? (Ja of Nee)")
+   if any(answer.lower() == f for f in ["Ja", 'J', 'ok', 'Y']):
+     print("Kom op dan!")
+     break
+
+   else:
+     i+=1
+     if i < 2:
+       print("Weet je het zeker?")
+     else:
+       print("Je moet!!!")
+
+  
+
+     
