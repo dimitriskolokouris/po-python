8d4fae365f4f070c04010f8acdb70299d7c5e5ea
Dimitris Kolokouris <95089320+dimitriskolokouris@users.noreply.github.com>
2022-06-07 13:53:26 +0000

    Woordenlijst toegevoegd

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
+#Woordenlijst
 import random
-word_list = ["informatica","galgje","python","programmeren","maartenscollege"]
-
-def getword(word_list):
-  word = random.choice(word_list)
-  return word.upper()
-
-
-def question():
-  x = 0
-  while x < 2:
-    answer = input("Hallo speler, wil je een potje Galgje spelen? (ja of nee)")
-    if any(answer.lower() == f for f in ["ja"]):
-      print("Veel speelplezier!")
-      break
-    
-    else:
-      x += 1
-      if x < 2:
-        print("Weet je het zeker?")
-
-
-question()
-      
+word_list = ["informatica","informatiekunde","spelletje","aardigheidje","scholier","fotografie","waardebepaling","specialiteit","verzekering","universiteit","heesterperk"]
